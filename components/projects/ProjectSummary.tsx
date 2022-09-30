80794898dc75ff37dd09d2d36333da6f3a6a45ac
Nick Musey <nmusey@gmail.com>
2022-09-29 17:44:23 -0700

    Finish markup for project summary

--- components/projects/ProjectSummary.tsx
+++ components/projects/ProjectSummary.tsx
@@ -4,10 +4,18 @@ export interface Props {
     project: Project;
 }
 
-export const ProjectSummary = (props: Props) => {
+export const ProjectSummary = ({project}: Props) => {
     return (
         <div>
-            <h1>{props.project.title}</h1>
+            <h1>{project.title}</h1>
+            <div>
+                <p>{project.summary}</p>
+                <div>
+                    { project.postUrl ? <a href={project.postUrl}>Blog Post</a> : null }
+                    { project.repoUrl ? <a href={project.repoUrl}>Source Code</a> : null }
+                    { project.repoUrl ? <a href={project.liveUrl}>Live Site</a> : null }
+                </div>
+            </div>
         </div>   
     );
 }
